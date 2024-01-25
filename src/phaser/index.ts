b1de4acccc9d1e34f485c15d97599ded5b602f72
Alan Romero Lucero <alanrolu@MacBook-Air-de-Alan.local>
2024-01-25 16:54:41 -0600

    Cambios

---
+++ src/phaser/index.ts
@@ -0,0 +1,12 @@
+import { AUTO, Game, Types } from 'phaser';
+import { First } from './Scenes';
+
+const config: Types.Core.GameConfig = {
+	type: AUTO,
+	backgroundColor: '#125555',
+	width: 800,
+	height: 600,
+	scene: First,
+};
+// Inicio del juego de phaser
+const game = new Game(config);

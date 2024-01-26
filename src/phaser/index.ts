c6670b7e8a8b0d1a480fb60e44d9e45513596f20
Alan Romero Lucero <alanrolu@MacBook-Air-de-Alan.local>
2024-01-26 10:02:36 -0600

    Agregando fuente y modificando estilos

--- src/phaser/index.ts
+++ src/phaser/index.ts
@@ -1,15 +1,16 @@
-import { AUTO, Game, Types } from 'phaser';
-import { First } from './Scenes';
+import { AUTO, Game, Types } from "phaser";
+import { First } from "./Scenes";
 
 const config: Types.Core.GameConfig = {
-	type: AUTO,
-	backgroundColor: '#125555',
-	width: 800,
-	height: 600,
-	scene: First,
+    type: AUTO,
+    backgroundColor: "#125555",
+    width: 800,
+    height: 600,
+    scene: First,
+    parent: "phaser-content",
 };
 // Inicio del juego de phaser
 
 const game = new Game(config);
 
-export { game }
+export { game };

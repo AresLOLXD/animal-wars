c503d71ff3ca85cfe434fd201a88537d534f8d94
Ares Ulises Juárez Martínez <aresulises8@hotmail.com>
2024-01-27 16:33:11 -0600

    Nuevos audios

--- src/phaser/Util/Functions.ts
+++ src/phaser/Util/Functions.ts
@@ -1,16 +1,19 @@
-import { Scene } from 'phaser';
-import { LOG_BASE } from './Constants';
+import { Scene } from "phaser";
+import { LOG_BASE } from "./Constants";
 export function calculateDimensions(scene: Scene) {
     return [
         scene.sys.canvas.width / 2,
         scene.sys.canvas.height / 2,
         scene.sys.canvas.width,
-        scene.sys.canvas.height
-    ]
-
+        scene.sys.canvas.height,
+    ];
 }
 
 export function calculateLogarithmTime(quantity: number, time: number) {
-    return ((Math.log(quantity) / Math.log(LOG_BASE)) * time * -1) + time;
+    return (Math.log(quantity) / Math.log(LOG_BASE)) * time * -1 + time;
 }
 
+//El valor debe ser un número entre 1 y 3
+export function calculateRate(quantity: number, time: number) {
+    return (Math.log(quantity) / Math.log(LOG_BASE)) * time * -1 + time;
+}
